2d97bf9ee604048a01abf1300f9f7074f7fae4f3
Gil Avignon <pro@gilavignon.com>
2022-01-13 11:48:26 +0100

    Test

--- src/authentication.ts
+++ src/authentication.ts
@@ -1,15 +1,18 @@
 import { Connection } from 'jsforce';
 
-export class Authentication{
-  connSfdc : Connection;
+export class Authentication {
+  connSfdc: Connection;
   // connOrg62 : Connection;
-  
+
   constructor(conn: Connection) {
-    console.log('\tHello! I\'m the Authentication Module! \n\tMy access token is ' + conn.accessToken);
+    console.log(
+      "\tHello! I'm the Authentication Module! \n\tMy access token is " +
+        conn.accessToken
+    );
     this.connSfdc = conn;
   }
 
-  getConnection() : Connection{
+  getConnection(): Connection {
     return this.connSfdc;
   }
 }
